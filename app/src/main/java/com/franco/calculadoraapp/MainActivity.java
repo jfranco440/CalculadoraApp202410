503e062e74cdfd8fd0a84a9891af2ce47473944a
jfran <jfranco440@unab.edu.co>
2024-02-21 16:58:19 -0500

    nuevo

--- app/src/main/java/com/franco/calculadoraapp/MainActivity.java
+++ app/src/main/java/com/franco/calculadoraapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.franco.calculadoraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.health.connect.datatypes.units.Length;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,14 +12,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etNum1, etNUM2;
+    private TextView tvResultado;
+    private Button btn_Sumar, btn_Restar, btn_Multiplicar, btn_Dividir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_Sumar, btn_Restar, btn_Multiplicar, btn_Dividir;
-        EditText etNum1, etNUM2;
-        TextView tvResultado;
 
         btn_Sumar = findViewById(R.id.btn_sumar);
         btn_Restar = findViewById(R.id.btn_restar);
@@ -36,7 +37,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickSumar(View miviewcita){
 
+        String num1String = etNum1.getText().toString();
+        Double num1 = Double.parseDouble(num1String);
+
+        String num2String = etNUM2.getText().toString();
+        Double num2 =Double.parseDouble(num2String);
+
+        Double resultado = num1 + num2;
+
+        tvResultado.setText(resultado.toString());
+
         Toast.makeText(this, "LE DI CLICK A SUMAR", Toast.LENGTH_SHORT).show();
     }
+
 
 }
